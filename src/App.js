907773ab72864f4ddbb60227ef61a56fed3bb3b7
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-06-28 21:01:12 -0400

    Added Loading component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
-import './App.css';
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading/Loading";
 
 function App() {
+  const [isStarted, setIsStated] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  function handleStartGame() {
+    setIsStated(true);
+  }
+
   return (
     <div className="App">
-      
+      {loading ? (
+        <Loading />
+      ): (
+        <div className="content-main">
+          
+        </div>
+      )}
     </div>
   );
 }
