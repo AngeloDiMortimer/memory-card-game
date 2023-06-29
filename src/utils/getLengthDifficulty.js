7d47d7c404e776cb31a55af31bb85c51a9d283fa
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-06-29 17:36:31 -0400

    added getLengthDifficulty.js

---
+++ src/utils/getLengthDifficulty.js
@@ -0,0 +1,22 @@
+const getDifficulty = (difficulty) => {
+    let LengthCards = 0;
+
+    switch (difficulty) {
+        case 'easy':
+            LengthCards = 3;
+            break;
+        case 'medium':
+            LengthCards = 4;
+            break;
+        case 'hard':
+            LengthCards = 5;
+            break;
+        default:
+            break;
+    }
+
+    return LengthCards;
+
+}
+
+export { getDifficulty };
